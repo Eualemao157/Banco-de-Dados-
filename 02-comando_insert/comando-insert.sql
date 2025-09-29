f545bd989fa49f974dfbaac650a3f8e41ece263c
GabrielluisbarbosaSouza <155595693+Eualemao157@users.noreply.github.com>
2025-09-29 17:50:54 -0300

    Create comando-insert.sql

---
+++ 02-comando_insert/comando-insert.sql
@@ -0,0 +1,5 @@
+-- Insere vários clientes de uma vez
+INSERT INTO clientes (nome, email) VALUES
+('João Silva', 'jorge@email.com'),
+('Mariana Claudia', 'mari@email.com'),
+('Carlos Magno', 'carlos@email.com');
